6b92ab996eb30150629abb41c6b122be849cc633
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-09-23 00:47:30 +0100

    function: Combine multiple reducers - split reducers into two - split state into two - split action into two - create a root reducer to combine both reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+// combine multiple reducers
+const combineReducers = redux.combineReducers;
 
 //redux action
-const BUY_CAKE = "Buy Cake";
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 //action creator
 const buyCake = () => {
@@ -11,21 +14,55 @@ const buyCake = () => {
     info: "first redux action",
   };
 };
+const buyIceCream = () => {
+  return {
+    type: BUY_ICECREAM,
+    info: "second redux action",
+  };
+};
 
 // reducer
 // (previousState, action) => newState
 
-const initialState = {
-  cakeID: 1,
+/* const initialState = {
+  numOfCakes: 20,
+  numOfIceCreams: 20,
+};*/
+
+// split state into two
+const initialCakeState = {
   numOfCakes: 20,
 };
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
 
+/*
+ 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state, //make a copy of the object
-        cakeID: state.cakeID + 1, //increment by one
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case BUY_ICECREAM:
+      return {
+        ...state, //make a copy of the object
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+*/
+
+// split reducer
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, //make a copy of the object
         numOfCakes: state.numOfCakes - 1,
       };
     default:
@@ -33,7 +70,25 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state, //make a copy of the object
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// combine reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 console.log("Initial State", store.getState());
 
 //subscribe
@@ -45,6 +100,8 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 //unsubscribe
 unsubscribe();
