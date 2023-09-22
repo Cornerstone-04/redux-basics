1d0c5a9d9bf6bc6a91edbd9d710cfa525c56a19a
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-09-22 14:01:29 +0100

    function: set action & reducer for app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("From index");
+//redux action
+const BUY_CAKE = "Buy Cake";
+
+//action creator
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: "first redux action",
+  };
+};
+
+// reducer
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 20,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, //make a copy of the object
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
